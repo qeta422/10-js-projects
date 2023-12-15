a9bc23b7319c02ba13367eac0812aa16be56c1d0
qeta422 <qetafetviashvili123@gmail.com>
2023-12-15 18:05:23 +0400

    project 5 javascript added

---
+++ js/main5.js
@@ -0,0 +1,14 @@
+function calculateTip() {
+    //get input type values
+    const billAmount = parseFloat(document.getElementById("billAmount").value);
+    const tipPercentage = parseFloat(document.getElementById("tipPercentage").value);
+
+    const tipAmount = billAmount * (tipPercentage/100);
+    const totalAmount = tipAmount + billAmount;
+    //display the result
+
+    document.getElementById('tipAmount').textContent = '$' + tipAmount.toFixed(2);
+    document.getElementById('totalAmount').textContent = '$' + totalAmount.toFixed(2);
+
+
+}
